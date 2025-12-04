e1d8a541f9002ed33771412eba5bd034c5b50d8b
Taslim Yusuf <Omotoyosi2210@gmail.com>
2025-12-04 12:14:12 +0100

    Deploy Modal fix

--- src/pages/MyTasks.jsx
+++ src/pages/MyTasks.jsx
@@ -3,7 +3,8 @@ import TaskTabs from "../components/TaskTabs";
 import TaskList from "../components/TaskList";
 import { useState, useEffect } from "react";
 import { getAllHabits } from "../api";
-import Modal from "../components/modal";
+// import Modal from "../components/Modal";
+import Modal from "../components/Modal"
 import CreateTask from "../components/CreateTask";
 
 const testTasks = [
